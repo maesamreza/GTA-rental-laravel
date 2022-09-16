9a0bb65af719451209b6ad8a255bef83b02de591
abdullahyafai <ayafai540@gmail.com>
2022-09-16 15:46:16 +0500

    steup done

---
+++ GTA-Rental/resources/views/Admin/layout/master.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>GTA-Rental | Admin</title>
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{asset('assets/css/app.min.css')}}">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/css/components.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/bundles/jqvmap/dist/jqvmap.min.css')}}">
+  <!-- Custom style CSS -->
+  <link rel="stylesheet" href="{{asset('assets/css/custom.css')}}">
+  <link rel='shortcut icon' type='image/x-icon' href='assets/img/favicon.ico' />
+</head>
+
+<body>
+  <div class="loader"></div>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+   @include('Admin.layout.header')
+   @include('Admin.layout.sidebar')
+   
+      @yield('content')
+     
+      @include('Admin.layout.footer')
+    </div>
+  </div>
+  <!-- General JS Scripts -->
+  <script src="{{asset('assets/js/app.min.js')}}"></script>
+  <!-- JS Libraies -->
+  <script src="{{asset('assets/bundles/apexcharts/apexcharts.min.js')}}"></script>
+  <script src="{{asset('assets/bundles/jqvmap/dist/jquery.vmap.min.js')}}"></script>
+  <script src="{{asset('assets/bundles/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
+  <!-- Page Specific JS File -->
+  <script src="{{asset('assets/js/page/index.js')}}"></script>
+  <!-- Template JS File -->
+  <script src="{{asset('assets/js/scripts.js')}}"></script>
+  <!-- Custom JS File -->
+  <script src="{{asset('assets/js/custom.js')}}"></script>
+</body>
+
+</html>
